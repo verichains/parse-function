de07dbf8d30586462283e0b162d2c71477bfbf3b
trongthai <trongthai.a4@gmail.com>
2023-03-10 15:44:01 +0700

    update: handle for function with multiple params

--- Parse.py
+++ Parse.py
@@ -1,9 +1,22 @@
 import json
+from eth_abi import decode_abi
 
 
 def read_file(file_path):
     with open(file_path, 'r') as f:
         return f.read()
+
+
+def print_arg(type_, value):
+    if type_.startswith('int') or type_.startswith('uint'):
+        return f'({type_})(0x{value})'
+    if type_ == 'address':
+        return f'({type_})(0x{value})'
+    if type_.startswith('bytes'):
+        return f'({type_})(0x{value.hex()})'
+    if type_.startswith('string'):
+        return f'({type_})("{value}")'
+    return f'({type_})({value})'
 
 
 def decode_function(input_data, output_data):
@@ -12,6 +25,36 @@ def decode_function(input_data, output_data):
     method_id_ = input_data[:10] if input_data else ''
     method = methods.get(method_id_, method_id_)
     method_str = f"{method}(0x{input_data[10:]})"
+    print(method_str)
+
+    if method.endswith(')'):
+        input_ = (input_data or '')[10:]
+        method_name = method.split('(')[0]
+
+        if method.count('(') == 1:
+            in_types = method.split('(')[1][:-1].split(',')
+        else:
+            in_types = []
+            iter_ = in_types
+            backs = []
+            for letter in method[method.index('(') + 1:]:
+                if letter == '(':
+                    iter_.append([])
+                    backs.append(iter_)
+                    iter_ = iter_[-1]
+                elif letter == ')':
+                    iter_ = backs.pop()
+                elif letter == ',':
+                    iter_.append("")
+                else:
+                    if not iter_ or not isinstance(iter_[-1], str):
+                        iter_.append("")
+                    iter_[-1] += letter
+
+        print(in_types)
+        in_decoded = decode_abi(in_types, bytes.fromhex(input_))
+        print(in_decoded)
+        method_str = f'{method_name}({", ".join(print_arg(type_, value) for type_, value in zip(in_types, in_decoded))})'
 
     if output_data:
         short_result = output_data.replace('0x0+', '0x')
