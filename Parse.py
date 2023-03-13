18260fc1eb05345319ef6a48bc98b593fdde7ee6
trongthai <trongthai.a4@gmail.com>
2023-03-13 14:44:13 +0700

    add main

--- Parse.py
+++ Parse.py
@@ -1,7 +1,14 @@
 import json
 from eth_abi import decode_abi
-import binascii
 import re
+
+
+# Example: input: 0xa9059cbb000000000000000000000000c663b28080e514662b469600bb3e69597fa1197400000000000000000000000000000000000000000000000000000000071596d2
+# Example: output: 0x0000000000000000000000000000000000000000000000000000000000000001
+# Return: transfer((address)(0xc663b28080e514662b469600bb3e69597fa11974), (uint256)(118855378)):0x1
+def main(input_data, output_data):
+    decoded_function = decode_function(input_data, output_data)
+    return decoded_function
 
 
 def read_file(file_path):
@@ -11,7 +18,7 @@ def read_file(file_path):
 
 def print_arg(type_, value):
     if type_.startswith('int') or type_.startswith('uint'):
-        return f'({type_})(0x{value})'
+        return f'({type_})({value})'
     if type_ == 'address':
         return f'({type_})({value})'
     if type_.startswith('bytes'):
@@ -84,12 +91,12 @@ def decode_function(input_data, output_data):
         # by iterating over each parameter and its corresponding value and combining them
         # into a comma-separated list enclosed in parentheses.
         method_str = f'{method_name}({", ".join(print_arg(type_, value) for type_, value in zip(in_types, in_decoded))})'
-        # method_str: transfer((address)(0x0xc663b28080e514662b469600bb3e69597fa11974), (uint256)(0x118855378))
+        # method_str: transfer((address)(0xc663b28080e514662b469600bb3e69597fa11974), (uint256)(0x118855378))
 
     # combine output_data
     if output_data:
         short_result = re.sub(r'0x0+', '0x', output_data)
         method_str += ':' + ('0x0' if short_result == '0x' else short_result)
 
-    # method_str: 'transfer((address)(0x0xc663b28080e514662b469600bb3e69597fa11974), (uint256)(0x118855378)):0x1'
+    # method_str: transfer((address)(0xc663b28080e514662b469600bb3e69597fa11974), (uint256)(118855378)):0x1
     return method_str
